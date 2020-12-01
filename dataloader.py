fc0f57acbef9725a6d5cd6260891a304962a1788
kawshik <kawshik98@gmail.com>
2020-12-01 00:04:21 -0500

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -32,166 +32,166 @@ class DataLoader:
         self.ids = []
         self.ego_car_images = []
         
-        if opt.training_method == 'finetune_sim':
-            costs_path = data_dir + ".costs"
-            costs = torch.load(costs_path)
-            self.costs = costs
-            print("costs: ",len(costs), len(costs[0]), costs[0][0].keys())
+#         if opt.training_method == 'finetune_sim':
+#             costs_path = data_dir + ".costs"
+#             costs = torch.load(costs_path)
+#             self.costs = costs
+#             print("costs: ",len(costs), len(costs[0]), costs[0][0].keys())
 
-            states_path = data_dir + ".states"
-            states = torch.load(states_path)
-            self.states = states
-            print("states: ",len(states), states[0].shape)
+#             states_path = data_dir + ".states"
+#             states = torch.load(states_path)
+#             self.states = states
+#             print("states: ",len(states), states[0].shape)
 
-            actions_path = data_dir + ".actions"
-            actions = torch.load(actions_path)
-            self.actions = actions
-            print("actions: ",len(actions), actions[0].shape)
+#             actions_path = data_dir + ".actions"
+#             actions = torch.load(actions_path)
+#             self.actions = actions
+#             print("actions: ",len(actions), actions[0].shape)
             
-            others_path = data_dir + ".others"
-            others = torch.load(others_path)
-            road_completed = others["road_completed"]
-            collided = others["collided"]
-            offscreen = others["offscreen"]
-            self.car_sizes = others["car_sizes"]
+#             others_path = data_dir + ".others"
+#             others = torch.load(others_path)
+#             road_completed = others["road_completed"]
+#             collided = others["collided"]
+#             offscreen = others["offscreen"]
+#             self.car_sizes = others["car_sizes"]
             
-            path = data_dir.split("/")
-            model_path = path[-1]
-            path = path[:-1]
-            path += ["videos_simulator", model_path]
-            images_path = "/".join(path)
+#             path = data_dir.split("/")
+#             model_path = path[-1]
+#             path = path[:-1]
+#             path += ["videos_simulator", model_path]
+#             images_path = "/".join(path)
 
-            episode_files = natsorted(os.listdir(images_path))
-#             print(episode_files[:5])
+#             episode_files = natsorted(os.listdir(images_path))
+# #             print(episode_files[:5])
 
-            episode_images = []
-            episode_ego_images = []
-            for episode in episode_files:
+#             episode_images = []
+#             episode_ego_images = []
+#             for episode in episode_files:
 
-                images = natsorted(os.listdir(images_path + "/" + episode + "/all/"))
-                if '.ipynb_checkpoints' in images:
-                    images.remove('.ipynb_checkpoints')
-                for image in images:
-                    image = Image.open(images_path + "/" + episode + "/all/" + image)
-#                     print(np.array(image).shape)
-                    image = np.array(image)
+#                 images = natsorted(os.listdir(images_path + "/" + episode + "/all/"))
+#                 if '.ipynb_checkpoints' in images:
+#                     images.remove('.ipynb_checkpoints')
+#                 for image in images:
+#                     image = Image.open(images_path + "/" + episode + "/all/" + image)
+# #                     print(np.array(image).shape)
+#                     image = np.array(image)
                     
-                    episode_images.append(image)
+#                     episode_images.append(image)
                     
-                self.images.append(episode_images)
+#                 self.images.append(episode_images)
                 
-                ego_images = natsorted(os.listdir(images_path + "/" + episode + "/ego/"))
-                if '.ipynb_checkpoints' in ego_images:
-                    ego_images.remove('.ipynb_checkpoints')
-                for ego_image in ego_images:
-                    ego_image = Image.open(images_path + "/" + episode + "/all/" + ego_image)
-#                     print(np.array(image).shape)
-                    ego_image = np.array(ego_image)
+#                 ego_images = natsorted(os.listdir(images_path + "/" + episode + "/ego/"))
+#                 if '.ipynb_checkpoints' in ego_images:
+#                     ego_images.remove('.ipynb_checkpoints')
+#                 for ego_image in ego_images:
+#                     ego_image = Image.open(images_path + "/" + episode + "/all/" + ego_image)
+# #                     print(np.array(image).shape)
+#                     ego_image = np.array(ego_image)
                     
-                    episode_ego_images.append(ego_image)
+#                     episode_ego_images.append(ego_image)
                     
-                self.ego_car_images.append(episode_ego_images)
+#                 self.ego_car_images.append(episode_ego_images)
                 
-            self.finetune_indx = []
-            for episode_i in range(len(self.images)):
-                if not road_completed[episode_i]:
-                    self.states[episode_i] = self.states[episode_i][-opt.npred + -opt.ncond:]
-                    self.actions[episode_i] = self.actions[episode_i][-opt.npred + -opt.ncond:]
-                    self.costs[episode_i] = self.costs[episode_i][-opt.npred + -opt.ncond:]
-                    self.images[episode_i] = self.images[episode_i][-opt.npred + -opt.ncond:]
-                    self.ego_car_images[episode_i] = self.ego_car_images[episode_i][-opt.npred + -opt.ncond:]
-                    self.finetune_indx += episode_i
+#             self.finetune_indx = []
+#             for episode_i in range(len(self.images)):
+#                 if not road_completed[episode_i]:
+#                     self.states[episode_i] = self.states[episode_i][-opt.npred + -opt.ncond:]
+#                     self.actions[episode_i] = self.actions[episode_i][-opt.npred + -opt.ncond:]
+#                     self.costs[episode_i] = self.costs[episode_i][-opt.npred + -opt.ncond:]
+#                     self.images[episode_i] = self.images[episode_i][-opt.npred + -opt.ncond:]
+#                     self.ego_car_images[episode_i] = self.ego_car_images[episode_i][-opt.npred + -opt.ncond:]
+#                     self.finetune_indx += episode_i
                     
-            combined_data_path = f'{data_dir}/all_data.pth'
-            torch.save({
-                    'images': self.images,
-                    'actions': self.actions,
-                    'costs': self.costs,
-                    'states': self.states,
-                    'ids': self.ids,
-                    'ego_car': self.ego_car_images,
+#             combined_data_path = f'{data_dir}/all_data.pth'
+#             torch.save({
+#                     'images': self.images,
+#                     'actions': self.actions,
+#                     'costs': self.costs,
+#                     'states': self.states,
+#                     'ids': self.ids,
+#                     'ego_car': self.ego_car_images,
+#                 }, combined_data_path)
+            
+#             self.n_episodes = len(self.images)
+#             print(f'Number of episodes: {self.n_episodes}')
+            
+            
+        
+#             splits_path = f'traffic-data/state-action-cost/data_{dataset}_v0' + '/splits.pth'
+        
+                
+#             car_sizes_path = f'traffic-data/state-action-cost/data_{dataset}_v0' + '/car_sizes.pth'
+
+#         else:
+        
+        for df in data_files:
+            combined_data_path = f'{data_dir}/{df}/all_data.pth'
+            if os.path.isfile(combined_data_path):
+                print(f'[loading data shard: {combined_data_path}]')
+                data = torch.load(combined_data_path)
+                self.images += data.get('images')
+                self.actions += data.get('actions')
+                self.costs += data.get('costs')
+                self.states += data.get('states')
+                self.ids += data.get('ids')
+                self.ego_car_images += data.get('ego_car')
+#                 print(len(data.get('images')))
+                data_images = data.get('images')
+
+#                 print(type(data_images),len(data_images),type(data_images[0]),len(data_images[0]),data_images[0].shape)
+            else:
+                print(data_dir)
+                images = []
+                actions = []
+                costs = []
+                states = []
+                ids = glob.glob(f'{data_dir}/{df}/car*.pkl')
+                ids.sort()
+                ego_car_images = []
+                for f in ids:
+                    print(f'[loading {f}]')
+                    fd = pickle.load(open(f, 'rb'))
+                    Ta = fd['actions'].size(0)
+                    Tp = fd['pixel_proximity_cost'].size(0)
+                    Tl = fd['lane_cost'].size(0)
+                    # assert Ta == Tp == Tl  # TODO Check why there are more costs than actions
+                    # if not(Ta == Tp == Tl): pdb.set_trace()
+                    images.append(fd['images'])
+                    actions.append(fd['actions'])
+                    costs.append(torch.cat((
+                        fd.get('pixel_proximity_cost')[:Ta].view(-1, 1),
+                        fd.get('lane_cost')[:Ta].view(-1, 1),
+                    ), 1),)
+                    states.append(fd['states'])
+                    ego_car_images.append(fd['ego_car'])
+
+                print(f'Saving {combined_data_path} to disk')
+                torch.save({
+                    'images': images,
+                    'actions': actions,
+                    'costs': costs,
+                    'states': states,
+                    'ids': ids,
+                    'ego_car': ego_car_images,
                 }, combined_data_path)
-            
-            self.n_episodes = len(self.images)
-            print(f'Number of episodes: {self.n_episodes}')
-            
-            
-        
-            splits_path = f'traffic-data/state-action-cost/data_{dataset}_v0' + '/splits.pth'
+                self.images += images
+                self.actions += actions
+                self.costs += costs
+                self.states += states
+                self.ids += ids
+                self.ego_car_images += ego_car_images
+
+        self.n_episodes = len(self.images)
+        print(f'Number of episodes: {self.n_episodes}')
+
+        f'traffic-data/state-action-cost/data_{dataset}_v0'
+
+        splits_path = data_dir + '/splits.pth'
         
                 
-            car_sizes_path = f'traffic-data/state-action-cost/data_{dataset}_v0' + '/car_sizes.pth'
-
-        else:
-        
-            for df in data_files:
-                combined_data_path = f'{data_dir}/{df}/all_data.pth'
-                if os.path.isfile(combined_data_path):
-                    print(f'[loading data shard: {combined_data_path}]')
-                    data = torch.load(combined_data_path)
-                    self.images += data.get('images')
-                    self.actions += data.get('actions')
-                    self.costs += data.get('costs')
-                    self.states += data.get('states')
-                    self.ids += data.get('ids')
-                    self.ego_car_images += data.get('ego_car')
-    #                 print(len(data.get('images')))
-                    data_images = data.get('images')
-
-    #                 print(type(data_images),len(data_images),type(data_images[0]),len(data_images[0]),data_images[0].shape)
-                else:
-                    print(data_dir)
-                    images = []
-                    actions = []
-                    costs = []
-                    states = []
-                    ids = glob.glob(f'{data_dir}/{df}/car*.pkl')
-                    ids.sort()
-                    ego_car_images = []
-                    for f in ids:
-                        print(f'[loading {f}]')
-                        fd = pickle.load(open(f, 'rb'))
-                        Ta = fd['actions'].size(0)
-                        Tp = fd['pixel_proximity_cost'].size(0)
-                        Tl = fd['lane_cost'].size(0)
-                        # assert Ta == Tp == Tl  # TODO Check why there are more costs than actions
-                        # if not(Ta == Tp == Tl): pdb.set_trace()
-                        images.append(fd['images'])
-                        actions.append(fd['actions'])
-                        costs.append(torch.cat((
-                            fd.get('pixel_proximity_cost')[:Ta].view(-1, 1),
-                            fd.get('lane_cost')[:Ta].view(-1, 1),
-                        ), 1),)
-                        states.append(fd['states'])
-                        ego_car_images.append(fd['ego_car'])
-
-                    print(f'Saving {combined_data_path} to disk')
-                    torch.save({
-                        'images': images,
-                        'actions': actions,
-                        'costs': costs,
-                        'states': states,
-                        'ids': ids,
-                        'ego_car': ego_car_images,
-                    }, combined_data_path)
-                    self.images += images
-                    self.actions += actions
-                    self.costs += costs
-                    self.states += states
-                    self.ids += ids
-                    self.ego_car_images += ego_car_images
-                
-            self.n_episodes = len(self.images)
-            print(f'Number of episodes: {self.n_episodes}')
-            
-            f'traffic-data/state-action-cost/data_{dataset}_v0'
-        
-            splits_path = data_dir + '/splits.pth'
-        
-                
-            car_sizes_path = data_dir + '/car_sizes.pth'
-            print(f'[loading car sizes: {car_sizes_path}]')
-            self.car_sizes = torch.load(car_sizes_path)
+        car_sizes_path = data_dir + '/car_sizes.pth'
+        print(f'[loading car sizes: {car_sizes_path}]')
+        self.car_sizes = torch.load(car_sizes_path)
             
         if os.path.exists(splits_path):
             print(f'[loading data splits: {splits_path}]')
@@ -259,10 +259,10 @@ class DataLoader:
             indx = self.valid_indx
         elif split == 'test':
             indx = self.test_indx
-        elif split == 'finetune_train':
+        else:# split == 'finetune_train':
             indx = list(self.finetune_dict.keys())
-        elif split == 'finetune_sim':
-            indx = self.finetune_indx
+#         elif split == 'finetune_sim':
+#             indx = self.finetune_indx
             
         self.total_instances[split] = 0
         
@@ -282,8 +282,8 @@ class DataLoader:
 #             print((min(self.images[index].size(0), self.states[index].size(0)) - (self.opt.ncond + self.opt.npred)),(min(self.images[index].size(0), self.states[index].size(0)) - (self.opt.ncond + self.opt.npred))//overlap)
             if split == 'finetune_train':
                 self.total_instances[split] += len(self.finetune_dict[index])
-            elif split == 'finetune_sim':
-                self.total_instances[split] += len(self.finetune_indx[index])
+#             elif split == 'finetune_sim':
+#                 self.total_instances[split] += len(self.finetune_indx[index])
             else:
                 self.total_instances[split] += (min(self.images[index].size(0), self.states[index].size(0)) - (self.opt.ncond + self.opt.npred))//overlap + 1
         
@@ -359,13 +359,13 @@ class DataLoader:
             if randomize:
 #                 print("indx: ",indx)
                 if all_batches:
-                    if split == 'finetune_train':
-                        if not self.t_finetune_dict:
-                            break
-                        else:
-                            s = self.random.choice(list(self.t_finetune_dict.keys()))
-                    elif split == 'finetune_sim':
-                        s = self.random.choice(indx)
+#                     if split == 'finetune_train':
+                    if not self.t_finetune_dict:
+                        break
+                    else:
+                        s = self.random.choice(list(self.t_finetune_dict.keys()))
+#                     elif split == 'finetune_sim':
+#                         s = self.random.choice(indx)
                 else:
                     s = self.random.choice(indx)
             else:
@@ -406,10 +406,10 @@ class DataLoader:
                 actions.append(self.actions[s][t : t + T].to(device))
                 states.append(self.states[s][t : t + T, 0].to(device))  # discard 6 neighbouring cars
                 costs.append(self.costs[s][t : t + T].to(device))
-                if split == 'finetune_sim':
-                    ids.append(self.ids[self.test_indx[s]])
-                else:
-                    ids.append(self.ids[s])
+#                 if split == 'finetune_sim':
+#                     ids.append(self.ids[self.test_indx[s]])
+#                 else:
+                ids.append(self.ids[s])
                 
                 ego_cars.append(self.ego_car_images[s].to(device))
                 
@@ -420,10 +420,10 @@ class DataLoader:
                     
                 time_slot = splits[-2]
                 car_id = int(re.findall(r'car(\d+).pkl', splits[-1])[0])
-                if split == 'finetune_sim':
-                    size = self.car_sizes[time_slot][car_id]
-                else:
-                    size = self.car_sizes[s]
+#                 if split == 'finetune_sim':
+#                     size = self.car_sizes[s]
+#                 else:
+                size = self.car_sizes[time_slot][car_id]
                 sizes.append([size[0], size[1]])
                 if all_batches:
                     episodes_indices[nb] = np.array([s,t])
